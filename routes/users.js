4faaa7f767b0a742476ee509f4307a176f4e9e60
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-07-16 13:33:51 -0600

    add a users POST endpoint

---
+++ routes/users.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const User = require('../models/user');
+
+const router = express.Router();
+
+/* ========== POST/CREATE AN ITEM ========== */
+router.post('/', (req, res, next) => {
+  const { fullname, username, password } = req.body;
+
+  const newUser = { fullname, username, password };
+
+  User.create(newUser)
+    .then(result => {
+      res
+        .location(`${req.originalUrl}/${result.id}`)
+        .status(201)
+        .json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
+module.exports = router;
